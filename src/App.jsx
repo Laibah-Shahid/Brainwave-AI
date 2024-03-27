ef9348e8794065a0271c7b9235c2f89133f1addb
Laibah-Shahid <laibahshahid18@gmail.com>
2024-03-27 21:29:35 +0500

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,23 @@ import Hero from "./components/Hero";
 import Pricing from "./components/Pricing";
 import Roadmap from "./components/Roadmap";
 import Services from "./components/Services";
+import  { useState, useEffect } from 'react';
+import {Circle} from 'react-preloaders';
 
 const App = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 2000); 
+  }, []);
   return (
     <>
+       <div className="App">
+      {isLoading && <Circle color="#f3f3f3" backgroundColor="#3498db" size={80} />}
+      <Header />  {/* Render Header component */}
+    </div>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
         <Header />
         <Hero />
